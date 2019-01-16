d0abc04b8ca8c567cd9de2ce23d297e1b64a1201
Eugene Chou <euchou@ucsc.edu>
2019-01-16 11:13:33 -0800

    added plugins

--- .hyper.js
+++ .hyper.js
@@ -139,7 +139,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-font-ligatures', 'hypercwd', 'hyper-pane'],
+  plugins: [
+    'hyper-font-ligatures',
+    'hypercwd',
+    'hyper-pane',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
