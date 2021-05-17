e79f4d62dd9f5cb957de0022f8c91a3f3ef66fdc
Eugene Chou <euchou@ucsc.edu>
2021-05-17 00:14:05 -0700

    fix homebrew pathing

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,4 +1,10 @@
 ## Paths
-
-export PATH=$PATH:/usr/local/bin/python
+export PATH=/usr/local/bin:/usr/local/sbin
+export PATH=/usr/local/opt/llvm/bin:$PATH
+export PATH=/usr/local/opt/flex/bin:$PATH
+export PATH=/usr/local/opt/bison/bin:$PATH
+export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin
 export PATH=$PATH:/Applications/LilyPond.app/Contents/Resources/bin
+
+export PATH=$PATH:$GOPATH/bin
+export GOPATH="$HOME/Code/go"
