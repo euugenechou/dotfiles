6d515a9d6b6316bf2484902eab452d73fca13f2b
Eugene Chou <euchou@ucsc.edu>
2023-05-06 06:22:27 +0000

    finally

--- configs/euchou/tartarus.nix
+++ configs/euchou/tartarus.nix
@@ -1,40 +1,8 @@
 { inputs, outputs, lib, config, pkgs, ... }:
 {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      # outputs.overlays.additions
-      # outputs.overlays.modifications
-      # outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
     };
   };
@@ -101,7 +69,7 @@
           };
         }
       ];
-      localVariables = {
+      sessionVariables = {
         EDITOR = "nvim";
       };
       shellAliases = {
@@ -126,9 +94,20 @@
       '';
     };
 
-  # Enable home-manager and git
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "Eugene Chou";
+    userEmail = "euchou@ucsc.edu";
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+    };
+  };
+
+  # Bootstrap home-manager
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
